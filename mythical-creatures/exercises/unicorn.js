5b52508618194084ed12b61f9abc9e4969caab31
Daniel O'Connell <danieloconnell.maker@gmail.com>
2021-10-18 18:18:59 -0700

    Initial Commit -Complete Unicorn

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,8 +1,17 @@
 class Unicorn {
-  constructor(name, color) {
+  constructor(name, color = 'white') {
     this.name = name;
     this.color = color;
   }
+  isWhite() {
+    if (this.color != 'white') {
+      return false
+    }
+  }
+  says(quote) {
+    return `**;* ${quote} *;**`
+  }
+
 }
 
 module.exports = Unicorn;
