ec1d75f1d9c1290525eba32f73132d1a8a51a861
Daniel O'Connell <danieloconnell.maker@gmail.com>
2021-10-19 23:23:08 -0700

    Completed dragon.js

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,36 @@
+class Dragon  {
+  constructor(name, rider){
+    this.name = name;
+    this.rider = rider;
+    this.hungry = true;
+    this.fed = 0
+  }
+  greet(rider){
+    return `Hi, ${this.rider}!`
+  }
 
+  eat(fed) {
+    this.fed++;
+    if (this.fed >= 3) {
+      this.hungry = false
+    }
+    }
+  }
+
+
+
+
+var smog = new Dragon()
+
+smog.eat()
+console.log("fed once",smog.fed)
+smog.eat()
+console.log("fed twice",smog.fed)
+smog.eat()
+console.log("fed thrice",smog.fed)
+console.log("Smog is hungry", smog.hungry)
+
+
+
+
+module.exports = Dragon;
