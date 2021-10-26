3717323a37e9f55671a1ec85c441b4bc0cb22c65
Daniel O'Connell <danieloconnell.maker@gmail.com>
2021-10-25 17:17:58 -0700

    Complete Medusa

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -1,2 +1,25 @@
 var Person = require('./person');
 var Statue = require('./statue');
+var newlyFree;
+
+class Medusa {
+  constructor(name) {
+    this.name = name;
+    this.statues = [];
+  }
+  gazeAtVictim(victim) {
+    if (this.statues.length < 3) {
+      this.statues.push(new Statue(victim.name));
+    } else {
+      var newlyFree = this.statues.shift();
+      console.log(this.statues.push(new Statue(victim.name)));
+      return new Person(newlyFree.name, 'relieved');
+    }
+
+
+
+  }
+}
+
+
+module.exports = Medusa;
